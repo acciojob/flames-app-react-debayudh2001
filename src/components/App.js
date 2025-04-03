7de1567a8e60aece81596f217ad36ec244139039
debayudh2001 <debayudh31@gmail.com>
2025-04-03 12:38:58 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -10,12 +10,40 @@ const App = () => {
     const [relationship, setRelationship] = useState("");
 
     function logic(str1, str2) {
-        let str = str1.toLowerCase() + str2.toLowerCase()
-        let arr = str.split("")
-        let set = new Set(arr)
-        let brr = [...set]
-        let newStr = brr.join("")
-        return newStr.length % 6
+        str1 = str1.toLowerCase()
+        str2 = str2.toLowerCase()
+        let map1 = new Map()
+        let map2 = new Map()
+        for (let v of str1) {
+            if (map1.has(v)) {
+                map1.set(v, map1.get(v) + 1)
+            } else {
+                map1.set(v, 1)
+            }
+        }
+        for (let v of str2) {
+            if (map2.has(v)) {
+                map2.set(v, map2.get(v) + 1)
+            } else {
+                map2.set(v, 1)
+            }
+        }
+        let obj1 = Object.fromEntries(map1)
+        let obj2 = Object.fromEntries(map2)
+        let count = 0
+        for (let char in obj1) {
+            if (obj2[char]) {
+                count += Math.abs(obj1[char] - obj2[char])
+            } else {
+                count += obj1[char]
+            }
+        }
+        for (let char in obj2) {
+            if (!obj1[char]) {
+                count += obj2[char]
+            }
+        }
+        return count % 6
     }
 
     function handleChange(e) {
@@ -31,22 +59,22 @@ const App = () => {
             return;
         }
         switch (val) {
-            case 0 :
+            case 0:
                 setRelationship("Siblings")
                 break;
-            case 1 :
+            case 1:
                 setRelationship("Friends")
                 break;
-            case 2 :
+            case 2:
                 setRelationship("Love")
                 break;
-            case 3 :
+            case 3:
                 setRelationship("Affection")
                 break;
-            case 4 :
+            case 4:
                 setRelationship("Marriage")
                 break;
-            case 5 :
+            case 5:
                 setRelationship("Enemy")
                 break;
             default:
